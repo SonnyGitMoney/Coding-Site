04a22b8c6b0e5921e0eba3260500e47ce89d82c5
SonnyGitMoney <sendsonnymail@gmail.com>
2022-10-21 11:30:43 -0700

    remove code, input other code

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+
+    let response = document.getElementById("animal").value;
+
+
+
+    if (response === "badger") {
+      document.getElementById("badger").removeAttribute("class");
+    }
+    else if (response === "lep") {
+      document.querySelector("div#lep").removeAttribute("class");
+    }
+    
+    else if (response === "wheel") {
+      document.querySelector("div#wheel").removeAttribute("class");
+    }
+    else {
+      document.querySelector("div#missed").removeAttribute("class");
+    }
+  
+  
+  };
+};
