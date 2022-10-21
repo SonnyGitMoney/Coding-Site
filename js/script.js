33294b866b14826bd6503db3261d1daa6e29586c
SonnyGitMoney <sendsonnymail@gmail.com>
2022-10-21 15:46:48 -0700

    removed bootstrap code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   const form = document.querySelector("form");
-  form.onsubmit = function(event) {
+  form.addEventListener("submit",function(event) {
     event.preventDefault();
     
     let response = document.getElementById("baddy").value;
