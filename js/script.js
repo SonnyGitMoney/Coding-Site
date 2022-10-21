c4126a6553b5fe74fa11d3d2e6ae757833388dac
SonnyGitMoney <sendsonnymail@gmail.com>
2022-10-21 12:07:40 -0700

    moved images

--- js/script.js
+++ js/script.js
@@ -2,25 +2,20 @@ window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = function(event) {
     event.preventDefault();
-
+    
     let response = document.getElementById("baddy").value;
 
-
-
-    if (response === "one") {
+    if (response === "1") {
       document.getElementById("one").removeAttribute("class");
     }
-    else if (response === "two") {
+    else if (response === "2") {
       document.querySelector("div#two").removeAttribute("class");
     }
-    
-    else if (response === "three") {
+    else if (response === "3") {
       document.querySelector("div#three").removeAttribute("class");
     }
     else {
       document.querySelector("div#missed").removeAttribute("class");
     }
-  
-  
   };
 };
